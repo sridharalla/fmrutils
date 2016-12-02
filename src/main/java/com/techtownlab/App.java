7c3012d415473f595577a7496898d83dff739015
sridhar alla <sridharalla@outlook.com>
2016-12-02 01:40:30 +0000

    first

---
+++ src/main/java/com/techtownlab/App.java
@@ -0,0 +1,18 @@
+package com.techtownlab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        Utils utils = new Utils();
+        utils.setProduct("myproduct");
+
+        System.out.println(utils.getProduct());
+    }
+}
---
+++ src/main/java/com/techtownlab/Utils.java
@@ -0,0 +1,28 @@
+package com.techtownlab;
+
+/**
+ * Created by Administrator on 12/2/2016.
+ */
+public class Utils {
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    private String product;
+    private String version;
+
+
+}
